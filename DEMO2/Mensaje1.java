a041d50e304cb82a9426fdbbc0b626b7cc359c3d
cdp055 <116390098+cdp055@users.noreply.github.com>
2023-04-18 22:01:26 -0500

    Add files via upload

---
+++ DEMO2/Mensaje1.java
@@ -0,0 +1,8 @@
+package DEMO2;
+
+public class Mensaje1 {
+    public static void main(String[] args) {
+        System.out.println("Loja, una ciudad llena de arte y naturaleza con 400322");
+}
+
+}
